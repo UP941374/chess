cc15e75406410ca79fb39aaa415217b21183249b
UP941374 <up941374@myport.ac.uk>
2021-01-18 15:40:01 +0000

    pieces now move

--- script.js
+++ script.js
@@ -5,33 +5,25 @@ let blackcolor = '#b3b3b3';
 
 window.addEventListener('resize', onresize);
 window.addEventListener('load', newgame);
-document.getElementById('canv').addEventListener('click', movepiece);
+document.getElementById('canv').addEventListener('mousedown', pickpiece);
+document.getElementById('canv').addEventListener('mouseup', droppiece);
 
 function drawboard(){
   let c = document.getElementById('canv');
   let ctx = c.getContext("2d");
-
   ch = window.innerHeight*0.95;
   cw = window.innerWidth*0.95;
-
   let bh = ch;
   let bw = cw;
-
   c.width = bh;
   c.height = c.width;
-
   if (ch > cw) {
-    console.log('pionowe')
     c.height =  cw;
     c.width = c.height;
     bh = cw;
-
   }
-
-
   let fieldsize = bh / boardsize;
   fsize = fieldsize;
-
   //draw board
   for (let i=0; i<boardsize; i++) {
       for (let j=0; j<boardsize; j++) {
@@ -41,7 +33,7 @@ function drawboard(){
   }
 };
 
-function drawpieces(){
+function drawpieces(x){
   for (var piece of pieces) {
     if (piece.ingame) {
       let c = document.getElementById('canv');
@@ -50,32 +42,72 @@ function drawpieces(){
       image.src = piece.img;
       image.id = piece.pos
       image.onload = function(piece){
-        ctx.drawImage(image,getcordx(this.id),getcordy(this.id),fsize,fsize); // Or at whatever offset you like
+        ctx.drawImage(image,getcordx(this.id),getcordy(this.id),fsize,fsize);
       };
     }
   }
 };
 
-function movepiece(x){
-  console.log(x.clientY,x.clientX)
+function pickpiece(x){
+  let c = document.getElementById('canv');
+  ch = c.height;
+  cw = c.width;
+  let posx = Math.round(x.clientX - ((window.innerWidth - c.width) / 2));
+  let posy = Math.round(x.clientY-10);
+  for (let piece of pieces) {
+    if (piece.pos == tocord(posx,posy)) {
+      piece.moving = true;
+    }
+  }
 };
 
+function droppiece(x){
+  let c = document.getElementById('canv');
+  ch = c.height;
+  cw = c.width;
+  let posx = Math.round(x.clientX - ((window.innerWidth - c.width) / 2));
+  let posy = Math.round(x.clientY-10);
+
+  for (let piece of pieces) {
+    if (piece.moving) {
+      if (legalmove(piece, piece.pos, tocord(posx,posy))) {
+        piece.pos = tocord(posx,posy)
+        piece.moving = false;
+      }
+    }
+  }
+  redraw();
+};
+
+function legalmove(p,o,n){
+  console.log(p.type +':'+ o + '->'+n)
+  return true;
+}
+
+function tocord(x,y){
+  let cordx = String.fromCharCode(Math.ceil(x/fsize+64));
+  let cordy = Math.ceil(y/fsize);
+  let cords = cordx+cordy;
+  return cords;
+}
+
 function getcordx(x){
-  let res = (x[0].charCodeAt(0)-65)*Math.floor(fsize);
-  return res;
+  return Math.round((x[0].charCodeAt(0)-65)*fsize);
 }
 
 function getcordy(x){
-  let res = (x[1]-1)*Math.floor(fsize);
-  return res;
+  return Math.round((x[1]-1)*fsize);
 }
 
 function onresize(){
-  drawboard();
-  drawpieces();
+  redraw()
 }
 
 function newgame(){
+  redraw()
+};
+
+function redraw(){
   drawboard();
   drawpieces();
 }
